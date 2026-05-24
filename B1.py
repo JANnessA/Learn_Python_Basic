2187afe2c28b900256539a51cf3fa9583728e241
JANnessA <tuyenletm31@gmail.com>
2026-05-24 21:58:06 +0700

    ex1

--- B1.py
+++ B1.py
@@ -1,6 +1,6 @@
 # EX1: Arithmetic Product and Conditional Logic
 # Practice Problem: Write a Python function that accepts two integer numbers. 
-# If the product of the two numbers is less than or equal to 1000, return their product; otherwise, return their sum.
+#   If the product of the two numbers is less than or equal to 1000, return their product; otherwise, return their sum.
 
 # Exercise Purpose: Learn basic control flow and the use of if-else statements. Understand how code decisions change output based on a mathematical threshold.
 
@@ -11,3 +11,16 @@
 #   The result is 600
 #   The result is 70
 
+def product_or_sum (num1, num2):
+    product = num1*num2
+
+    if product <= 1000:
+        return product
+    else:
+        return num1+num2
+    
+result = product_or_sum(20, 30)
+print(f'the first result is {result}')
+
+result = product_or_sum(40, 30)
+print(f'the first result is {result}')
